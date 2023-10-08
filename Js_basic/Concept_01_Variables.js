c519ff8392c11b3156fac849fd56aede9222ce4f
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2023-10-08 21:36:39 +0000

    variables done

---
+++ Js_basic/Concept_01_Variables.js
@@ -0,0 +1,18 @@
+const accountId=1000
+// const vairible value can not be changed
+
+let accountEmail="jitendra@gmail.com"
+var accountPassward="0090"
+
+accountCity="kuchaman city"
+//in javascript without any declear variable type we declear its value
+
+let accountState
+//if we want to change the const value then its show an error...but let and var we changed its value
+// accountId=111
+console.log(accountId);
+
+
+//new one-> we see the all variable value in one console then we use" console.table([accountId,accountEmail,accountPassward,accountCity,accountState])""
+console.table([accountId,accountEmail,accountPassward,accountCity,accountState])
+//show result in tabular formate
