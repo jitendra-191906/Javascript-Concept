8df5cd4bb461736808fc385b97133560a90b0a2a
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2023-10-08 22:12:50 +0000

    datatypes Done

---
+++ Js_basic/Concept_02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all code newly version of js
+
+//data types
+/*
+number
+bigint
+string
+bolean
+null->standalona value
+undefined->value not defined
+*/
+
+let name="jitu";
+let val=null;
+let state;
+console.log(typeof name)
+console.log(typeof state)
+console.log(typeof val)
+
+/*
+Note2 
+typeof null=object
+typeof undefined=undefined
+*/
