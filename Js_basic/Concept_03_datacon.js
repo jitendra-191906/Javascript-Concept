89f58cc1a9934d526eed99981dc4c97f85eb86d5
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2023-10-09 12:23:38 +0000

    datatype operation done

---
+++ Js_basic/Concept_03_datacon.js
@@ -0,0 +1,23 @@
+//number to string basic,advaced
+let numb="12ab"
+// console.log(typeof numb)
+let s1=Number(numb)
+// console.log(typeof s1)
+
+// console.log(s1)
+//NaN = TYPE SHOW US THE NUMBER BUT THIS NOT A NUMBER
+
+let a=null
+// console.log(typeof a)
+let b =Number(a)
+// console.log(typeof b)
+// console.log(b)
+
+
+//Operation//
+let s11 =1 + 1
+let s2= 1 + 1 + "2" +2
+let s3=s11 + s2;
+console.log(s3);
+
+console.log(+true)//1
