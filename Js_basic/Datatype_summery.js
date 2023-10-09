48aded21ffbc5ebae8547735045d671cfd0ca649
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2023-10-09 23:50:23 +0000

    data type summery

---
+++ Js_basic/Datatype_summery.js
@@ -0,0 +1,6 @@
+//on the basic of access of data type and store that data...datatype in two types
+// 1)premitive Data
+//string,number,bolean,null,undefined,symbol,bigint
+
+// 2)non premitive data(reference )
+//array,functions,objects
