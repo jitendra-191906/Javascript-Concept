c50b847d473b780ed9c80c7211a4f881f8929ca1
Jitendra kumar <79419900+jitendra-191906@users.noreply.github.com>
2023-10-10 22:34:14 +0000

    memory concept

--- Js_basic/Datatype_summery.js
+++ Js_basic/Datatype_summery.js
@@ -4,3 +4,8 @@
 
 // 2)non premitive data(reference )
 //array,functions,objects
+//=====+++++++++++=======++++++memory
+//+++++++two typs+++++++
+
+// stack memory(make a copy not use original value...chnage in copy value not in real value)''''_>premitive data type
+// heap memory(make a reference of original value....changes make gives effect)>> non premitive data type
